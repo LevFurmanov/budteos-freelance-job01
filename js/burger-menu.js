d45183d301436b3d74e994b3bada1046ef328aaa
Lev <levthepredator@gmail.com>
2023-05-13 01:16:24 +0300

    burger-menu-done

---
+++ js/burger-menu.js
@@ -0,0 +1,13 @@
+const btnBurger = document.querySelector('.header-burger');
+const btnBurgerMobile = document.querySelector('.header-burger-mobile');
+let menu = document.querySelector('.header-mobile-menu');
+
+btnBurger.addEventListener('click', () => {
+  if (menu !== 'block') {
+    menu.style.display = 'block';
+  }
+});
+
+btnBurgerMobile.addEventListener('click', () => {
+  menu.style.display = 'none';
+});
