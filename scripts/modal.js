af7c239cdee6aeb2eb6783daa35ee28e13c2abd1
Lev <levthepredator@gmail.com>
2023-05-21 18:58:54 +0300

    Delete modal.js

--- scripts/modal.js
+++
@@ -1,9 +0,0 @@
-$(".modal-btn").on("click", (e) => {
-    e.preventDefault();
-    $("#ex1").modal({
-        escapeClose: false,
-        clickClose: true,
-        showClose: false, 
-        fadeDuration: 200 
-      });
-})
